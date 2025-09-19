1f8fc3bb36adb83f8f54f19433e3c77895ebd267
SOUFIYAN BENALLAL <41121758+soufiyanbenallal@users.noreply.github.com>
2025-09-20 05:12:23 +0800

    Add Docusaurus configuration and initial documentation structure
    - Created essential Docusaurus files including client-modules.js, globalData.json, and various plugin configurations.
    - Added initial documentation pages for components, tutorials, and blog posts.
    - Introduced CSS cascade layers for styling and established a debug plugin for development.
    - Set up site metadata and routing for improved navigation and content management.

---
+++ docs/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/user/Documents/beyonders/sirius/docs/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/user/Documents/beyonders/sirius/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/user/Documents/beyonders/sirius/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@18.3.24_acorn@8.15.0_react-dom@18.3.1_react@18.3_zhfemengzggpjx2bbhft5vyeqi/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/user/Documents/beyonders/sirius/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@18.3.24_acorn@8.15.0_react-dom@18.3.1_react@18.3_zhfemengzggpjx2bbhft5vyeqi/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/user/Documents/beyonders/sirius/docs/src/css/custom.css"),
+];
